edc34da8a200d5ea6a96b148c5708503c01d57eb
Ch Tejaswi <phanitejaswi267@gmail.com>
2022-08-20 02:08:01 +0530

    deployed files

---
+++ deployment_content/src/resources/__init__.py
@@ -0,0 +1,8 @@
+import os
+
+pwd = os.getcwd()
+
+pkl_wordtoix_file = pwd+"/src/resources/wordtoix.pkl"
+pkl_ixtoword_file = pwd+"/src/resources/ixtoword.pkl"
+
+print("Initiated pkl_wordtoix_file and  pkl_ixtoword_file")
